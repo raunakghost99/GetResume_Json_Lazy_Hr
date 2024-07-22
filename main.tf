66839d0a6dbc9c29317c5819c90318c7cd269df8
Raunak-ghost <raunakbhargava8@gmail.com>
2024-07-22 12:44:25 +0530

    fixed name issues

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "iam" {
   source             = "./modules/iam"
-  dynamodb_table_arn = module.dynamodb.dynamodb_table_arn
+  dynamodb_table_arn = module.dynamo_db.dynamodb_table_arn
   lambda_role_name   = var.lambda_role_name
   lambda_policy_name = var.lambda_policy_name
   region = var.region
-  dynamodb_table_name=module.dynamodb.dynamodb_table_name
+  dynamodb_table_name=module.dynamo_db.dynamodb_table_name
 }
 
 module "dynamo_db" {
@@ -15,20 +15,20 @@ module "dynamo_db" {
 
 module "lambda_function" {
   source                         = "./modules/lambda_function"
-  lambda_role_name               = module.iam_role.lambda_role_name
+  lambda_role_name               = module.iam.lambda_role_name
   function_name                  = var.function_name
-  lambda_role_arn                = module.iam_role.lambda_role_arn
-  dynamodb_table_name            = module.dynamodb.dynamodb_table_name
-  dynamodb_policy_attachment_arn = module.iam_role.dynamodb_policy_attachment_arn
-  cloudwatch_policy_attachment_arn = module.iam_role.cloudwatch_policy_attachment_arn
+  lambda_role_arn                = module.iam.lambda_role_arn
+  dynamodb_table_name            = module.dynamo_db.dynamodb_table_name
+  dynamodb_policy_attachment_arn = module.iam.dynamodb_policy_attachment_arn
+  cloudwatch_policy_attachment_arn = module.iam.cloudwatch_policy_attachment_arn
 
 }
 
 
 module "api_gateway" {
   source                   = "./modules/api_gateway"
-  resume_lambda_invoke_arn = module.lambda.resume_lambda_invoke_arn
-  lambda_function_name     = module.lambda.lambda_function_name
+  resume_lambda_invoke_arn = module.lambda_function.resume_lambda_invoke_arn
+  lambda_function_name     = module.lambda_function.lambda_function_name
   region                   = var.region
   
 }
